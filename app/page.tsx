37c22bf05db710326f7df75f1a764df125877dc7
sakhyat <sakhyatkabir1@gmail.com>
2023-07-15 00:35:37 +0600

    added title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@
 
 const HomePage: React.FC = () => {
   return (
-    <div>Home</div>
+    <div className="max-w-5xl mx-auto p-10">
+      <div className="title">
+        <h1 className="text-4xl border-b py-5">Task Management Application</h1>
+      </div>
+    </div>
   )
 }
 
