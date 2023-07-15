8f340ba104b166005071160842541049e67eb587
sakhyat <sakhyatkabir1@gmail.com>
2023-07-15 17:02:58 +0600

    added storecontext

---
+++ contexts/StoreContext.tsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+import { TodoStoreType } from "../models/TodoStore";
+
+const StoreContext = createContext<TodoStoreType | null>(null);
+
+export const useStore = (): TodoStoreType => {
+  const store = useContext(StoreContext);
+  if (!store) {
+    throw new Error("useStore must be used within a StoreProvider.");
+  }
+  return store;
+};
+
+export default StoreContext
