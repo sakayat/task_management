2e47f31a1023dce8defc9cd566b512074d9937c3
sakhyat <sakhyatkabir1@gmail.com>
2023-07-15 17:04:57 +0600

    added storeprovider

---
+++ contexts/StoreProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+import TodoStore from "@/models/TodoStore";
+import StoreContext from "./StoreContext";
+import { ReactNode } from "react";
+
+type StoreProviderProps = {
+  children: ReactNode;
+};
+
+const todoStore = TodoStore.create();
+
+const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => {
+  return (
+    <StoreContext.Provider value={todoStore}>{children}</StoreContext.Provider>
+  );
+};
+
+export default StoreProvider;
