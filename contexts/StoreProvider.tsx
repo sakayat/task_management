d7a3ef965eeb7c2bda2e8444005663d3342951d6
sakhyat <sakhyatkabir1@gmail.com>
2023-07-17 09:57:41 +0600

    get data from localstorage

--- contexts/StoreProvider.tsx
+++ contexts/StoreProvider.tsx
@@ -1,7 +1,14 @@
 "use client";
 import TodoStore from "@/models/TodoStore";
 import StoreContext from "./StoreContext";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+
+interface Todo {
+  id?: number;
+  title: string;
+  description: string;
+  selected: string;
+}
 
 type StoreProviderProps = {
   children: ReactNode;
@@ -10,6 +17,16 @@ type StoreProviderProps = {
 const todoStore = TodoStore.create();
 
 const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => {
+  useEffect(() => {
+    const getTodos: Todo[] = JSON.parse(
+      localStorage.getItem("todos") as string
+    );
+    if (getTodos) {
+      getTodos.forEach((todo) => {
+        todoStore.addTodo(todo);
+      });
+    }
+  }, [todoStore]);
   return (
     <StoreContext.Provider value={todoStore}>{children}</StoreContext.Provider>
   );
